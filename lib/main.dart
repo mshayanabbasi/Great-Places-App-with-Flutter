6106663159b81f824f11e899ac191848f6712ebb
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-28 23:00:20 +0500

    Add provider

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './providers/great_places.dart';
+import 'package:provider/provider.dart';
+import './screens/place_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlaceListScreen(),
+      ),
+    );
+  }
+}
