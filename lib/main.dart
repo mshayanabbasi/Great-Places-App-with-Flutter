3bb4b4c2df9142d630a7b4bd238c8fe49bdb2d01
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-29 11:49:34 +0500

    Add Place Screen route

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import './providers/great_places.dart';
 import 'package:provider/provider.dart';
 import './screens/place_list_screen.dart';
-
+import './screens/add_place_screen.dart'  ;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,12 +12,16 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Great Places',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
           accentColor: Colors.amber,
         ),
         home: PlaceListScreen(),
+        routes: {
+          AddPlaceScreen.routeName : (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
